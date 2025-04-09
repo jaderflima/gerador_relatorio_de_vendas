3c2e94db2a0e7a74a1218acd90d1188f530dc813
IsabellaAlvarezn <isabellaalvarezandrade@gmail.com>
2025-04-08 22:03:26 -0300

    Implementação dos gráficos

--- page_02.py
+++ page_02.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import base64 
+import pandas as pd
+import altair as alt
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 st.set_page_config(page_title="DATANExT", layout="centered" )
 
@@ -74,7 +79,35 @@ st.sidebar.markdown("### Ordem dos Eixos")
 
 ordem_eixos = st.sidebar.radio("Selecione uma opção:" ,["Gráfico vendedor", "Gráfico Gerente"])
 
-upload_csv = st.session_state.get('upload_csv', 'teste')
+#upload_csv = st.session_state.get('upload_csv', 'teste')#
+
+#gráficos
+csv_caminho = r"Base de dados - base_farmacia_esportiva_3000.csv"
+
+try:
+    data = pd.read_csv(csv_caminho)
+    st.success("Dados carregados com sucesso!")
+    st.dataframe(data)
+
+    data['Data_Venda'] = pd.to_datetime(data['Data_Venda'], errors='coerce')
+    data['Quantidade_Vendida'] = pd.to_numeric(data['Quantidade_Vendida'], errors='coerce')
+    data['Preço_Unitário'] = pd.to_numeric(data['Preço_Unitário'], errors='coerce')
+    data['Valor_Total2'] = data['Quantidade_Vendida'] * data['Preço_Unitário']
+    data['AnoMes'] = data['Data_Venda'].dt.to_period('M')
+    vendas_por_mes = data.groupby('AnoMes')['Valor_Total2'].sum()
+    vendas_por_mes.index = vendas_por_mes.index.astype(str)
+    st.write("###  Total vendido por mês")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    vendas_por_mes.plot(ax=ax)
+    ax.set_title('Total vendido por mês')
+    ax.set_xlabel('Ano-Mês')
+    ax.set_ylabel('Valor Total')
+    ax.grid(True)
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
+
+except Exception as e:
+    st.error(f"Erro ao carregar os dados: {e}")
 
 #Rodapé
 st.markdown("""<hr><div style= 'text-align: center;font-size: small;'>©Todos os direitos reservados.</div>""", unsafe_allow_html=True)
